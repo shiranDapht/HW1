2a746cdc8015040c010a0f01e84f844c5c74aaf5
Shiran <shiranDapht1@gmail.com>
2021-12-18 18:38:07 +0200

    +tests

--- mux4_test.sv
+++ mux4_test.sv
@@ -1,10 +1,33 @@
 // 4->1 multiplexer test bench template
 module mux4_test;
 
-// Put your code here
-// ------------------
+    logic d0, d1, d2, d3, z;
+    logic [1:0] sel;
 
+mux4# ()
+mux4_tb(
+    .d0(d0),
+    .d1(d1),
+    .d2(d2),
+    .d3(d3),
+    .sel(sel),
+    .z(z)
+);
 
-// End of your code
+initial begin
+    d0 = 1;
+    d1 = 1;
+    d2 = 0;
+    d3 = 1;
+    sel[0] = 0;
+    sel[1] = 0;
+
+    #30
+    sel[1] = 1;
+
+    #30
+    sel[1] = 0;
+    
+    end
 
 endmodule
