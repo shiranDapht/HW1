e6f9a1c30ae6bd984e192667b1ddeeacfd3223d9
aviel360 <aviel3600@gmail.com>
2021-12-18 15:25:59 +0200

    Begin

--- mux2.sv
+++ mux2.sv
@@ -1,27 +1,60 @@
 // 2->1 multiplexer template
-
 module mux2 (
     input logic d0,          // Data input 0
     input logic d1,          // Data input 1
     input logic sel,         // Select input
     output logic z           // Output
 );
-
-NOT #(
-    .Tpdhl(6), .Tpdlh(1)
-) not_gate (
-        .A(a), .Z(z)
-);
-
-OR2 #(
-    .Tpdhl(10), .Tpdlh(10)
-) or_gate (
-        .A(a), .B(b), .Z(z)
-);
-
-not_gate not_gate_0  ();
-
+	logic out1, out2, out3, out4, out5, out6, out7;
+	NOT#(
+		.Tpdhl(6), .Tpdlh(1)
+		) g1 (
+			.A(d1),
+			.Z(out1)
+	);
+	NOT #(
+		.Tpdhl(6), .Tpdlh(1)
+		) g2 (
+			.A(sel),
+			.Z(out2)
+	);
+	NOT #(
+		.Tpdhl(6), .Tpdlh(1)
+		) g3 (
+			.A(d0),
+			.Z(out3)
+	);
+	OR2 #(
+		.Tpdhl(10), .Tpdlh(10)
+		) g4 (
+			.A(out1),
+			.B(out2),
+			.Z(out4)
+	);
+	OR2 #(
+		.Tpdhl(10), .Tpdlh(10)
+		) g5 (
+			.A(sel),
+			.B(out3),
+			.Z(out5)
+	);
+	NOT #(
+		.Tpdhl(6), .Tpdlh(1)
+		) g6 (
+			.A(out4),
+			.Z(out3)
+	);
+	NOT #(
+		.Tpdhl(6), .Tpdlh(1)
+		) g7 (
+			.A(out5),
+			.Z(out3)
+	);
+	OR2 #(
+		.Tpdhl(10), .Tpdlh(10)
+		) g8 (
+			.A(out6),
+			.B(out7),
+			.Z(z)
+	);
 endmodule
-
-
-
--- mux4.sv
+++ mux4.sv
@@ -1,8 +1,10 @@
-// 2->1 multiplexer template
-module mux2 (
+// 4->1 multiplexer template
+module mux4 (
     input logic d0,          // Data input 0
     input logic d1,          // Data input 1
-    input logic sel,         // Select input
+    input logic d2,          // Data input 2
+    input logic d3,          // Data input 3
+    input logic [1:0] sel,   // Select input
     output logic z           // Output
 );
 
